0fe4bd78f0cc71d65276ee5c2b0aafb8038f6651
Desy Armariena <desy.armariena@gmail.com>
2018-10-30 17:21:20 +0700

    Dictionary sort of strings

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,27 @@
 const readline = require('readline');
 // your code here to initialize the program and take user input
 
+function Dictionary(arrWords) {
+
+    for (var i = 1; i < arrWords.length; i++) {
+
+        var pembanding = arrWords[i];
+        //cek mundur
+        var j = i - 1;
+        while (j >= 0 && arrWords[j] > pembanding) {            
+            var temp = arrWords[j];            
+            arrWords[j+1] = arrWords[j];
+            arrWords[j] = pembanding            
+            j--;
+        }
+    }
+    return arrWords.join();    
+}
+
+var arrOfWord = ['makan', 'duduk', 'tidur', 'terbang'];
+console.log(Dictionary(arrOfWord));
+
+arrOfWord = ['anggi', 'angga', 'ani', 'adi'];
+console.log(Dictionary(arrOfWord));
+
 module.exports = Dictionary
