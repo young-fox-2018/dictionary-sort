bd2653e715ed6e9df2c01b50f4ce61de5dc88969
Zhang <zhang@android-42520aa2424d51bc.localdomain>
2018-10-30 16:40:42 +0700

    kumpul dic sort

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,36 @@
 const readline = require('readline');
-// your code here to initialize the program and take user input
 
-module.exports = Dictionary
+function dictionary(input){
+ 
+  // var result = []
+  // var tampung ;
+
+  // for(var i = 0 ; i < input.length ; i++){
+  //   tampung = input[i]
+  //   result.push(tampung)
+  //   if(input[i+1] < tampung){
+  //     result.push(input[i+1])
+  //   }
+  // }
+  // return result
+  var str ;
+  var index = 0 ;
+
+  for(var i = 0 ; i < input.length ; i++){
+    str = input[i];
+    index = i-1;
+
+    while(index >= 0 && input[index] > str){
+      input[index+1] = input[index];
+      index = index-1
+    }
+
+    input[index+1] = str
+  }
+  return input.join(',')
+}
+
+console.log(dictionary(['makan' , 'duduk' , 'tidur' , 'terbang']));
+console.log(dictionary(['anggi' , 'angga' , 'ani' , 'adi']));
+
+// module.exports = Dictionary
