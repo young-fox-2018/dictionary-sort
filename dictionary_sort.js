3c0146bf7f46a00616ba808e820cf5d1067dd62c
Atras Tudhi <atras.r@gmail.com>
2018-10-30 17:35:13 +0700

    dictionary sort challange done

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,26 @@
 const readline = require('readline');
 // your code here to initialize the program and take user input
+function dictionarySort(dic) {
 
-module.exports = Dictionary
+    for(let i = 0; i < dic.length; i++) {
+        let small = dic[i]
+        let smallIndex = i
+        let temp
+        for(let j = i; j < dic.length; j++) {
+            if(dic[j] < small) {
+                small = dic[j]
+                smallIndex = j
+            }
+        }
+        temp = dic[i]
+        dic[i] = small
+        dic[smallIndex] = temp
+    }
+
+    return dic.join()
+}
+
+console.log(dictionarySort(['makan', 'duduk', 'tidur', 'terbang']))
+console.log(dictionarySort(['anggi', 'angga', 'ani', 'adi']))
+
+// module.exports = Dictionary
