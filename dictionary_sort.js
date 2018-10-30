ff6eacff0cdf23807e7a3cf7520bc4da8394a361
Kevin Wijaya <Kevin@Macbooknyaa.localdomain>
2018-10-30 16:03:08 +0700

    submitting this excercise

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,30 @@
 const readline = require('readline');
 // your code here to initialize the program and take user input
 
-module.exports = Dictionary
+function dict(arr){
+    var alphabet ="abcdefghijklmnopqrstuvwxyz"
+    
+    for(var i = 0; i < arr.length ; i++){
+        var min  = alphabet.indexOf(arr[i][0])
+        var index 
+        for(var j = i; j < arr.length; j++){
+            if(alphabet.indexOf(arr[j][0]) < min){
+                min = alphabet.indexOf(arr[j][0]);
+                index = j;
+            }
+        }
+        console.log(min)
+        console.log(index)
+        console.log("====")
+        var temp = arr[i];
+        arr[i] = arr[index];
+        arr[index] = temp
+    }
+    return arr
+}
+
+
+
+console.log(dict(["bruno", "zebra","apple","carly"]))
+
+module.exports = dict
