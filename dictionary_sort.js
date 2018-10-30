36ad68135f1a2a9290034189c78a578c4452e0b3
taqiabdulaziz <svx.annexiv@gmail.com>
2018-10-30 16:08:24 +0700

    add dictionary sort

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,25 @@
-const readline = require('readline');
-// your code here to initialize the program and take user input
+// const readline = require('readline');
+const readline = ["makan", "duduk", "tidur", "terbang"];
+function kamus(arr) {
+    var counter = 0
+    while (counter < arr.length) {
+        for (let i = 1; i < arr.length; i++) {
+            if (arr[i] < arr[i-1]) {
+                var a = arr[i]
+                arr[i] = arr[i-1]
+                arr[i-1] = a
+            }
 
-module.exports = Dictionary
+        }
+
+        counter++
+    }
+    return arr
+}
+console.log(kamus(readline));
+
+
+
+
+
+// module.exports = Dictionary
