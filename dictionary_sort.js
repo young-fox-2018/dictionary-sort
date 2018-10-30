1573488cd0bead1f70b45421a1ac9f4b8f90cc9f
taqiabdulaziz <svx.annexiv@gmail.com>
2018-10-30 18:06:39 +0700

    add dictionary

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -2,16 +2,12 @@
 const array = ["makan", "duduk", "tidur", "terbang"];
 function kamus(arr) {
     var counter = 0
-    while (counter < arr.length) {
-        for (let i = 1; i < arr.length; i++) {
-            if (arr[i] < arr[i-1]) {
-                var a = arr[i]
-                arr[i] = arr[i-1]
-                arr[i-1] = a
-            }
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            
+            
         }
-
-        counter++
+        
     }
     return arr
 }
