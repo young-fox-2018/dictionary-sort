3bab09398aed089aabf292559430ec35611cd5f2
imanuel vicky <imanuvi@imanuels-MBP.localdomain>
2018-10-30 16:00:19 +0700

    merge sort implementation

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,15 +1,52 @@
 const readline = require('readline');
 // your code here to initialize the program and take user input
 
-function Dictionary (arr) {
-    // using insertion sort
-    for (let i = 0; i < arr.length; i++) {
-        while (arr[i-1] > arr[i]) { // change > value to change sorting direction
-            let temp_arr_i_min = arr[i - 1]; arr[i-1] = arr[i]; arr[i] = temp_arr_i_min; i--;
+        function merge(left, right) {
+            result = [];
+            left_index = 0; right_index = 0;
+            while (left_index < left.length && right_index < right.length) {
+                if (left[left_index] < right[right_index]) {
+                    result.push(left[left_index])
+                    left_index += 1
+                } else {
+                    result.push(right[right_index])
+                    right_index += 1
+                }
+            }
+            if (right.length > left.length) {
+                let loop_the_rest = right.length - left.length;
+                for (let i = right.length - (1 + loop_the_rest); i < right.length; i++) {
+                    result.push(right[i])
+                } 
+            } else {
+                let loop_the_rest = left.length - left.length;
+                for (let i = left.length - (1 + loop_the_rest); i < left.length; i++) {
+                    result.push(left[i])
+                } 
+            }
+            return result
         }
-     }
-    return arr.join()
-}
+
+        // merge sort
+
+        function mergeSort(arr) {
+            if (arr.length <= 1) {
+                return arr
+            }
+            let mid = Math.floor(arr.length / 2)
+            let left = arr.slice(0,mid)
+            let right = arr.slice(mid, arr.length)
+
+            left = mergeSort(left)
+            right = mergeSort(right)
+            return merge(left, right)
+        }
+
+        function Dictionary (arr) {
+            return mergeSort(arr)
+        }
+
+
 
 
 console.log(Dictionary(['makan','duduk','tidur','terbang']))
