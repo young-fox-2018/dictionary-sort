d6df1d186a13ddd209175af1bcae2ab75125e125
maitreya <bluespider529@gmail.com>
2018-10-30 18:36:44 +0700

    finished dictionary sort challenge

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,46 @@
-const readline = require('readline');
-// your code here to initialize the program and take user input
+// const readline = require('readline');
 
-module.exports = Dictionary
+function dictionary(input){
+
+    //bubble sort!
+
+    // var temp = 0;
+    // for(var i = 0; i < input.length; i++){
+    //     // console.log(input[i])
+    //     if(input[i] > input[i+1]){
+    //         temp = input[i];
+    //         input[i] = input[i+1];
+    //         input[i+1] = temp;
+    //     }
+    // }
+    // var result = ""
+    // return result += input
+
+    //insetion sort!
+    
+    for(var i = 0; i < input.length; i++){
+        // console.log(arr[i])
+        var firstIndex = i;
+        var firstValue = input[i];
+        // console.log(firstIndex)
+        var temp = 0;
+        for(var j = firstIndex - 1; j >= 0; j--){
+          // console.log(arr[firstIndex] < arr[j])
+          if(input[firstIndex] < input[j]){
+            temp = input[j];
+            input[j] = firstValue;
+            input[firstIndex] = temp;
+            firstIndex = firstIndex - 1;
+          }
+        }
+      }
+      return input.join(",");
+}
+
+var arrOfWord = ["makan", "duduk", "tidur", "terbang"];
+var arrOfName = ["anggi", "angga", "ani", "adi"];
+
+console.log(dictionary(arrOfWord));
+console.log(dictionary(arrOfName));
+
+// module.exports = Dictionary
