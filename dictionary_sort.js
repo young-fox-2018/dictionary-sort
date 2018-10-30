33b41894b3cf7813dc7c3fd84b66f3b85e4e86a2
abednegolubis <lubisabednego@gmail.com>
2018-10-30 17:42:54 +0700

    submit challenge : dictionary sort

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,22 @@
-const readline = require('readline');
-// your code here to initialize the program and take user input
+function dictionary(array){
+    for (let i = 0; i < array.length; i++) {
+        var currentMin = array[i]
+        var indexCurrentMin = i
+        var temp = 0
+        for (let j = i+1; j < array.length; j++) {
+            if(array[j] <  currentMin){
+                currentMin = array[j]
+                indexCurrentMin = j
+            }
+        }
+        if(array[i] > currentMin) {
+            temp = array[i]
+            array[i] = array[indexCurrentMin]
+            array[indexCurrentMin] = temp
+        }
+    }
+    return array
+}
 
-module.exports = Dictionary
+console.log(dictionary(['makan','duduk','tidur','terbang']))
+console.log(dictionary(['anggi','angga','ani','adi']))
