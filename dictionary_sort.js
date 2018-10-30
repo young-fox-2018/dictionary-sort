cfda932fb39448ee4c7c3f00e7b11cc56a8f012a
Sulistiyono <sulistiyono@Sulistio-MacBook-Pro.local>
2018-10-30 11:35:15 +0700

    done

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,20 @@
 const readline = require('readline');
 // your code here to initialize the program and take user input
+function Dictionary(params) {
+    for (let i = 0; i < params.length; i++) {
+        let temp = ""
+        for (let j = 0; j < params.length; j++) {
+            if (params[i] < params[j]) {
+                temp = params[i]
+                params[i] = params[j]
+                params[j] = temp
+            }
+        }
+    }
+    return params.toString()
+}
+console.log(Dictionary(["makan", "duduk", "tidur", "terbang"]));
+console.log(Dictionary(["anggi", "angga", "ani", "adi"]));
+
 
 module.exports = Dictionary
